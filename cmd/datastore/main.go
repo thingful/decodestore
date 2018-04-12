4996e2355f948ca9229f66143b2c232d2e050c96
Sam Mulube <sam@thingful.net>
2018-04-12 14:40:40 +0100

    Move server initialization and run function into library

--- cmd/datastore/main.go
+++ cmd/datastore/main.go
@@ -1,45 +1,9 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"net/http"
-	"time"
-
-	"github.com/twitchtv/twirp"
-
-	"github.com/thingful/datastore/pkg/impl/datastoreserver"
-	"github.com/thingful/datastore/pkg/rpc/datastore"
-)
-
-type contextKey string
-
-func (c contextKey) String() string {
-	return fmt.Sprintf("DECODE.%s", string(c))
-}
-
-var (
-	reqStartTimestampKey = contextKey("request.start")
-)
+import "github.com/thingful/datastore/pkg/server"
 
 func main() {
-	server := datastoreserver.NewServer()
+	server := server.NewServer(":8080")
 
-	hooks := &twirp.ServerHooks{}
-
-	hooks.RequestReceived = func(ctx context.Context) (context.Context, error) {
-		return context.WithValue(ctx, reqStartTimestampKey, time.Now()), nil
-	}
-
-	hooks.ResponseSent = func(ctx context.Context) {
-		start, ok := ctx.Value(reqStartTimestampKey).(time.Time)
-		if ok {
-			dur := time.Now().Sub(start)
-			fmt.Printf("Request completed, duration: %v\n", dur)
-		}
-	}
-
-	twirpHandler := datastore.NewEncryptedStoreServer(server, hooks)
-
-	http.ListenAndServe(":8080", twirpHandler)
+	server.Start()
 }
---
+++ pkg/server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/thingful/datastore/pkg/impl/datastoreserver"
+	"github.com/thingful/datastore/pkg/rpc/datastore"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+func NewServer(addr string) *Server {
+	rpc := datastoreserver.NewServer()
+	twirpHandler := datastore.NewEncryptedStoreServer(rpc, nil)
+
+	srv := &http.Server{Addr: addr, Handler: twirpHandler}
+
+	return &Server{
+		srv: srv,
+	}
+}
+
+func (s *Server) Start() {
+	log.Println("Starting server...")
+
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+
+	go func() {
+		if err := s.srv.ListenAndServe(); err != nil {
+			log.Printf("listen error: %s\n", err)
+		}
+	}()
+
+	<-stopChan
+	log.Println("Shutting down server...")
+
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	s.srv.Shutdown(ctx)
+}
